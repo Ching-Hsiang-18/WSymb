aa22ea90847b2650b316a3e7316df4bd03024af6
Celestine SAUVAGE <csauvage@Emeraude-hpc-1.lifl.fr>
2019-02-26 15:20:11 +0100

    add .h

--- include/CFTree.h
+++ include/CFTree.h
@@ -20,7 +20,46 @@ namespace otawa { namespace cftree {
 	class DAGVNode;
 	class DAGHNode;
 	class DAGNode;
-	class DAG;
+	class DAG
+	{
+		private:
+			// Set of DAG nodes
+			std::vector<DAGNode*> all;
+
+			// Entry node for DAG
+			DAGNode *n_start;
+
+			// Exits nodes for DAG
+			std::vector<DAGNode*> n_ends;
+
+			// Virtual node next
+			DAGNode *n_next;
+
+		public:
+			// Iterator for nodes in DAG
+			std::vector<DAGNode*>::const_iterator iter();
+			std::vector<DAGNode*>::const_iterator end();
+
+			// Getter and setter nodes
+			void addNode(DAGNode *s);
+			void setStar(DAGNode *s);
+			void addEnd(DAGNode *s);
+			void setNext(DAGNode *s);
+
+			// Getter for particulars nodes
+			DAGNode *getStart();
+			DAGNode *getNext();
+			DAGNode *getElement(unsigned i);
+			DAGNode *getiEnd(int i);
+
+			// Iterator for nodes with an exit edge
+			std::vector<DAGNode*>::const_iterator iter_e();
+			std::vector<DAGNode*>::const_iterator end_e();
+
+			// Check if a node "elem" is in the DAG or not
+			bool find_node(DAGNode *elem);
+			~DAG();
+	};
 
 	class CFTreeLeaf;
 	class CFTreeAlt;
@@ -31,18 +70,21 @@ namespace otawa { namespace cftree {
 	Identifier<DAGHNode*> DAG_HNODE("otawa::cftree:DAG_HNODE");
 	Identifier<DAGBNode*> DAG_BNODE("otawa::cftree:DAG_BNODE");
 
+// method to print the dag
 	io::Output &operator<<(io::Output &o, const DAGVNode &n);
 	io::Output &operator<<(io::Output &o, DAGNode &n);
 	io::Output &operator<<(io::Output &o, const DAGBNode &n);
 	io::Output &operator<<(io::Output &o, const DAGHNode &n);
 	io::Output &operator<<(io::Output &o, DAG &n);
 
+// method to print the cft
 	io::Output &operator<<(io::Output &o, CFTree &n);
 	io::Output &operator<<(io::Output &o, CFTreeSeq &n);
 	io::Output &operator<<(io::Output &o, CFTreeLeaf &n);
 	io::Output &operator<<(io::Output &o, CFTreeAlt &n);
 	io::Output &operator<<(io::Output &o, CFTreeLoop &n);
 
+// method to describe a cft in a .dot
 	std::string write_tree(CFTree &n, unsigned int *lab);
 	std::string write_tree(CFTreeSeq &n, unsigned int *lab);
 	std::string write_tree(CFTreeLeaf &n, unsigned int *lab);
@@ -71,6 +113,11 @@ class DAGHNode;
 extern Identifier<DAGBNode*> DAG_BNODE;
 extern Identifier<DAGHNode*> DAG_HNODE;
 
+//declaration propriete CFTRee
+// extern Identifier<CFTree*> CFTREE;
+
+
+// TODO : declarer ici les classes pour manipuler le CFTree
 
 } }
 
