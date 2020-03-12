ebd4d7517cd7ad1680a1dbf287f175638efb0d34
Clement Ballabriga <Clement.Ballabriga@univ-lille.fr>
2020-03-12 11:52:45 +0100

    Ca marche tant qu'on met pas le cache

--- example/pwcet_instantiator.c
+++ example/pwcet_instantiator.c
@@ -8,8 +8,10 @@
 
 int b = 10;
 
-int param_value(int param_id) {
+int has_param = 0;
 
+int param_value(int param_id) {
+  has_param = 1;
   if (param_id == 1) {
     return b;
   } else abort();
@@ -27,10 +29,14 @@ int param_loop_bound(int loop_id) {
 int main(void) {
     loopinfo_t li = {.hier = loop_hierarchy, .bnd = param_loop_bound };
     
-    for (int i = 0; i < 20; i++) {
-      b = i;
-      long long wcet = evaluate(&f, &li, NULL, NULL);
-      printf("for param value %d, wcet: %lld\n", i, wcet);
+    if (has_param) {
+      for (int i = 0; i < 20; i++) {
+        b = i;
+        long long wcet = evaluate(&f, &li, NULL, NULL);
+        printf("for param value %d, wcet: %lld\n", i, wcet);
+      }
+    } else {
+      printf("wcet: %lld\n", evaluate(&f, &li, NULL, NULL));
     }
 
 }
